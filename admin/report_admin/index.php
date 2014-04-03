76c32d2cb3a3075437a682af219fe5bab14e186c
radin-reth <radin.reth@gmail.com>
2014-04-04 00:47:35 +0700

    custom report admin index

--- admin/report_admin/index.php
+++ admin/report_admin/index.php
@@ -54,7 +54,53 @@
         chart.draw(data, options);
       }
     </script>
+    
+    <script>
+      function printData()
+      {
+         var divToPrint=document.getElementById("counter_report_s_adm");
+         newWin= window.open("");
+         newWin.document.write(divToPrint.innerHTML);
+         newWin.print();
+         newWin.close();
+      }
+        
+      var tableToExcel = (function() {
+        var uri = 'data:application/vnd.ms-excel;base64,'
+          , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+          , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
+          , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
+        return function(table, name) {
+          if (!table.nodeType) table = document.getElementById(table)
+          var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+          window.location.href = uri + base64(format(template, ctx))
+        }
+      })();
 
+      $(document).ready(function(){
+        $("#print-report").click(function(){
+          printData();
+        });
+        $("#export-report").click(function(){
+          tableToExcel('counter_report_s_adm', 'W3C Example Table');
+        });
+
+      });
+    </script>
+    <style type="text/css">
+      #print-report, #export-report{
+        border: 1px solid #CCCCCC;
+        border-radius: 3px;
+        color: #787878;
+        cursor: pointer;
+        float: right;
+        font-weight: bold;
+        margin-left: 10px;
+        padding: 3px;
+        text-transform: uppercase;
+
+      }
+    </style>
 </head>
 
 <body>
@@ -64,6 +110,8 @@
 	<div id="content_s_all_adm">
     	<div id="counter_report_search_adm">
         	Filters by:&nbsp;&nbsp;[<a href="">Month</a>]&nbsp;&nbsp;<!--[<a href="">Year</a>]-->
+          <span id="print-report">Print</span>
+          <span id="export-report">Export to Excel</span>
         </div>
       <div id="counter_report_s_adm">
       		<!--income statement-->
